527313345ab1ce3f8d7305e60808bfb7080ab976
brayansebastianrojasreyes <brayannrojass1996@gmail.com>
2022-12-01 22:47:48 -0500

    proyecto

--- semana1/TALLERES JAVASCRIPT/semana_12/js_ejercicio_1.js
+++
@@ -1,8 +0,0 @@
-     
-
-        var dias_semana=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
-
-        var Dia=dias_semana[5];//sabado
-        
-          console.log(Dia);
-           
